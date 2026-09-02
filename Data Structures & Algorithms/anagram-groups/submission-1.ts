a2348d8a32edf4d8c2aaaf9284cea825425374ce
reubenreid <45557196+reubenreid@users.noreply.github.com>
2026-09-02 13:40:12 +0100

    Add: anagram-groups - submission-1

---
+++ Data Structures & Algorithms/anagram-groups/submission-1.ts
@@ -0,0 +1,20 @@
+class Solution {
+    /**
+     * @param {string[]} strs
+     * @return {string[][]}
+     */
+    groupAnagrams(strs: string[]): string[][] {
+        const result = strs.reduce((acc, curr) => {
+            const chars = curr.split("").sort().join();
+            if (acc[chars]) {
+                acc[chars].push(curr)
+            } else {
+                acc[chars] = [curr];
+            }
+
+            return acc;
+        }, {});
+
+        return Object.values(result)
+    }
+}
